5ca094a9a093e8a45f93375e36e2dc5e528c2e13
FelipeCherry3 <felipeelias609@hotmail.com>
2024-06-11 17:22:30 -0300

    final - JavaDoc

--- src/main/java/ContaBancaria.java
+++ src/main/java/ContaBancaria.java
@@ -1,15 +1,29 @@
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+/**
+ Classe responsável por declarar e implementar os métodos sugeridos pelo Cucumber
+ @author Felipe Elias RA 224475
+ **/
 
 public class ContaBancaria {
+
+    /**
+     * Step: Given Um cliente especial com saldo atual de {int} reais
+     * @param int1 Saldo do cliente
+     * @return saldo Ou cucumber.PendingExeption();
+     */
     @Given("Um cliente especial com saldo atual de {int} reais")
     public void um_cliente_especial_com_saldo_atual_de_reais(Integer int1) {
         // Write code here that turns the phrase above into concrete actions
         System.out.print(int1);
         throw new io.cucumber.java.PendingException();
     }
-
+    /**
+     *Step: When for solicitado um saque no valor de {int} reais
+     * @param int1 Valor solicitado
+     * @return {boolean} retorna {true} se pode realizar o saque ou {false}
+     * */
     @When("for solicitado um saque no valor de {int} reais")
     public void for_solicitado_um_saque_no_valor_de_reais(Integer int1) {
         // Write code here that turns the phrase above into concrete actions
@@ -17,32 +31,55 @@ public class ContaBancaria {
         throw new io.cucumber.java.PendingException();
     }
 
+    /**
+     *
+     * @param int1 Valor solicitado
+     * @return void
+     * */
     public void deve_efetuar_o_saque_e_atualizar_o_saldo_da_conta_para_reais(Integer int1) {
         // Write code here that turns the phrase above into concrete actions
         System.out.print(int1);
         throw new io.cucumber.java.PendingException();
     }
+    /**
+     * Step: Then check more outcomes
+    *  Checa outcomes
+     */
     @Then("check more outcomes")
     public void check_more_outcomes() {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
 
+    /**
+     * Step: Then eve efetuar o saque e atualizar o saldo da conta para {int} reais
+     * @param arg0 Valor do Saque
+     */
     @Then("deve efetuar o saque e atualizar o saldo da conta para {int} reais")
     public void deveEfetuarOSaqueEAtualizarOSaldoDaContaParaReais(int arg0) {
-        System.out.print(arg0);
     }
 
+
+    /**
+     * Step: Given Um cliente comum com saldo atual de {int} reais
+     * @param arg0 Saldo
+     */
     @Given("Um cliente comum com saldo atual de {int} reais")
     public void umClienteComumComSaldoAtualDeReais(int arg0) {
-        System.out.print(arg0);
+
     }
 
+    /**
+     * Step: When Solicitar um saque de {int} reais
+     * @param arg0 Saldo
+     */
     @When("Solicitar um saque de {int} reais")
     public void solicitarUmSaqueDeReais(int arg0) {
-        System.out.print(arg0);
     }
 
+    /**
+     * Step: Then não deve efetuar o saque e deve retornar a mensagem Saldo Insuficiente
+     */
     @Then("não deve efetuar o saque e deve retornar a mensagem Saldo Insuficiente")
     public void nãoDeveEfetuarOSaqueEDeveRetornarAMensagemSaldoInsuficiente() {
         System.out.println("Saldo Insuficiente");
--- src/test/java/Arquivo_teste.feature
+++ src/test/java/Arquivo_teste.feature
@@ -24,16 +24,16 @@ Feature: Cliente faz saque de dinheiro Como um cliente,
 
   @tag1
   Scenario: Cliente especial com saldo negativo
-    Given Um cliente especial com saldo atual de -200 reais
-    When for solicitado um saque no valor de 100 reais
-    Then deve efetuar o saque e atualizar o saldo da conta para -300 reais
+    Given Um cliente especial com saldo atual de -500 reais
+    When for solicitado um saque no valor de 330 reais
+    Then deve efetuar o saque e atualizar o saldo da conta para -830 reais
     And check more outcomes
 
 
   @tag2
   Scenario Outline: Cliente comum com saldo negativo
-    Given Um cliente comum com saldo atual de -200 reais
-    When Solicitar um saque de 200 reais
+    Given Um cliente comum com saldo atual de -2550 reais
+    When Solicitar um saque de 100 reais
     Then não deve efetuar o saque e deve retornar a mensagem Saldo Insuficiente
     Examples:
       |  |
